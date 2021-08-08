81604681efbbf382c62ad062b7c7c43f615850d2
“Salma” <h.n222@yahoo.com>
2021-08-08 14:55:53 -0400

    second push

---
+++ src/package1/GitClass.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class GitClass {
+
+    public static void testMethod(){
+        System.out.println("Git practice1");
+    }
+
+    public static void main(String[] args) {
+        testMethod();
+    }
+}
